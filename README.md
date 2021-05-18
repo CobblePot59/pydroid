4d2260c5cb5a7826763cb75d95d8bbf8a4582af2
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2021-05-18 21:03:24 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+**Manage your sdks and emulators in a light and simplified way using a webui.**
---
+++ sql/emulator.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS [emulator] (
+    [name] VARCHAR NULL,
+    [image] VARCHAR NULL
+);
+
+-- INSERT INTO emulator VALUES ('test','system-images;android-29;default;x86_64');
