4c12182f4973d096e67638a30d952c25dfb5ebca
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2023-02-28 21:25:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,14 @@ def isAdmin():
         is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
     return is_admin
 
-
 if not isAdmin():
     print(colored('Launch this app with elevated priviledges', 'red'))
+    exit()
+
+jdk = subprocess.run('java -version', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+if jdk.returncode != 0:
+    print(colored('Please verify your java installation', 'red'))
     exit()
 
 
@@ -27,8 +32,6 @@ sdkhome = str(Path.home()/'Android/sdk-home')
 download = 'tmp'
 
 if platform.system() == 'Darwin':
-    # PLATFORM
-    pt = 'https://dl.google.com/android/repository/platform-tools-latest-darwin.zip'
     # CMDLINE-TOOLS
     pkg = re.findall(r"commandlinetools-mac-[\d]{7}_latest\.zip", requests.get('https://developer.android.com/studio#command-tools').text)[0]
     tools = 'https://dl.google.com/android/repository/'+pkg
@@ -38,15 +41,11 @@ if platform.system() == 'Darwin':
     haxm = pkg.replace('windows', 'macosx')
     _os = 'Darwin'
 elif platform.system() == 'Linux':
-    # PLATFORM
-    pt = 'https://dl.google.com/android/repository/platform-tools-latest-linux.zip'
     # CMDLINE-TOOLS
     pkg = re.findall(r"commandlinetools-linux-[\d]{7}_latest\.zip", requests.get('https://developer.android.com/studio#command-tools').text)[0]
     tools = 'https://dl.google.com/android/repository/'+pkg
     _os = 'Linux'
 elif platform.system() == 'Windows':
-    # PLATFORM
-    pt = 'https://dl.google.com/android/repository/platform-tools-latest-windows.zip'
     # CMDLINE-TOOLS
     pkg = re.findall(r"commandlinetools-win-[\d]{7}_latest\.zip", requests.get('https://developer.android.com/studio#command-tools').text)[0]
     tools = 'https://dl.google.com/android/repository/'+pkg
@@ -101,13 +100,6 @@ def downloadSDK():
                     zf.extractall(sdkenv+'/haxm')
                     zf.close()
                     os.remove(download+'/haxm.zip')
-        # PLATFORM
-        os.makedirs(sdkenv+'/platforms')
-        wget.download(pt, download+'/platform.zip')
-        zf = ZipFile(download+'/platform.zip')
-        zf.extractall(sdkenv)
-        zf.close()
-        os.remove(download+'/platform.zip')
         # CMDLINE-TOOLS
         os.makedirs(sdkenv+'/cmdline-tools')
         wget.download(tools, download+'/tools.zip')
@@ -119,6 +111,14 @@ def downloadSDK():
         # LICENSES
         shutil.copytree('licenses', sdkenv+'/licenses')
         print(colored('\nsdk-env files is now downloaded', 'green'))
+        # BUILD-TOOLS
+        sdklist = subprocess.check_output(f'{sdkenv}/cmdline-tools/latest/bin/sdkmanager --list', shell=True, encoding="437")
+        global _build
+        _build = sorted(re.findall(r'build-tools;(.*?)[\s\n]', sdklist), reverse=True)[0]
+        subprocess.run(f'{sdkenv}/cmdline-tools/latest/bin/sdkmanager --install "build-tools;{_build}"', shell=True)
+        # PLATFORM-TOOLS
+        subprocess.run(f'{sdkenv}/cmdline-tools/latest/bin/sdkmanager --install "platform-tools"', shell=True)  
+
     else:
         print(colored('sdk-env files is already present', 'green'))
 
@@ -143,7 +143,7 @@ def installHypervisor():
     elif _os == 'Windows':
         if Path(sdkenv+'/haxm').exists() is True:
             try:
-                cmd = subprocess.check_output('cmd /c '+sdkenv+'/haxm/silent_install.bat -v')
+                cmd = subprocess.check_output('cmd /c '+sdkenv+'/haxm/silent_install.bat -v', encoding="437")
                 print(colored('haxm is installed', 'green'))
             except subprocess.CalledProcessError:
                 print(colored('haxm is not installed, this step will take a little time...', 'yellow'))
@@ -151,7 +151,7 @@ def installHypervisor():
                 print(colored('haxm is now installed', 'green'))
         elif Path(sdkenv+'/gvm').exists() is True:
             try:
-                cmd = subprocess.check_output('cmd /c '+sdkenv+'/gvm/silent_install.bat -v')
+                cmd = subprocess.check_output('cmd /c '+sdkenv+'/gvm/silent_install.bat -v', encoding="437")
                 print(colored('gvm is installed', 'green'))
             except subprocess.CalledProcessError:
                 print(colored('gvm is not installed, this step will take a little time...', 'yellow'))
@@ -172,16 +172,19 @@ def installEnvironment():
     spath = os.environ['PATH']
 
     if _os == 'Darwin' or _os == 'Linux':
-        upath = ':{_}/emulator:{_}/emulator/bin64:{_}/platform-tools:{_}/cmdline-tools/latest/bin'.format(_=sdkenv)
+        upath = ':{_}/emulator:{_}/emulator/bin64:{_}/platform-tools:{_}/build-tools/{_build}:{_}/cmdline-tools/latest/bin'.format(_=sdkenv, _build=_build)
         if not upath in os.environ['PATH']:
             os.environ['PATH'] = spath+upath
             subprocess.run('echo \'PATH="'+spath+upath+'"\' >> /etc/profile', shell=True)
             subprocess.run('source /etc/profile', shell=True)
     elif _os == 'Windows':
-        wpath = '{_}\\emulator;{_}\\emulator\\bin64;{_}\\platform-tools;{_}\\cmdline-tools\\latest\\bin'.format(_=sdkenv)
+        import winreg
+
+        key = winreg.QueryValueEx(winreg.OpenKey(winreg.HKEY_CURRENT_USER, "Environment"), "Path")[0]
+        wpath = ';{_}\\emulator;{_}\\emulator\\bin64;{_}\\platform-tools;{_}\\build-tools\\{_build};{_}\\cmdline-tools\\latest\\bin'.format(_=sdkenv, _build=_build)
         if not wpath in os.environ['PATH']:
             os.environ['PATH'] = spath+wpath
-            subprocess.run('setx PATH "'+spath+wpath+'" /m', shell=True)
+            subprocess.run('setx PATH "'+key+wpath+'"', shell=True)
     print(colored('Your environement variable has been modified', 'green'))
 
 
