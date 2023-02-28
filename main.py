97cce2ee451e6a33bb3e9e925d129f9e9857d9bb
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2023-02-28 21:24:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 from views import app
 from app import downloadSDK, installHypervisor, installEnvironment, updateSDK
-from termcolor import colored
-import subprocess
 
 
 if __name__ == '__main__':
-    jdk = subprocess.run('java -version', shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    if jdk.returncode != 0:
-        print(colored('Please verify your java installation', 'red'))
-    else:
-        downloadSDK()
-        installHypervisor()
-        installEnvironment()
-        updateSDK()
-        app.run(host='0.0.0.0', port=80)
+    downloadSDK()
+    installHypervisor()
+    installEnvironment()
+    updateSDK()
+    app.run(host='0.0.0.0', port=80)
